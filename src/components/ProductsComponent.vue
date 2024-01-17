4a1678d4bc9e5f767cd7d53fdc99ba4870f0b423
Paulo <paulo.ugarte@alarcongroup.us>
2024-01-17 11:36:02 -0500

    first commit

---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <h1>Lista de Productos</h1>
+  <div class="relative overflow-x-auto">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                  <th scope="col" class="px-6 py-3">
+                      Product name
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                      Price
+                  </th>
+              </tr>
+          </thead>
+          <tbody  v-for="product in products" :key="product.id">
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ product.name }}
+                  </th>
+                  <td class="px-6 py-4">
+                    {{ product.price }}
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+
+  <div>
+    <form class="max-w-sm mx-auto">
+      <div class="mb-5">
+        <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Producto</label>
+        <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+      </div>
+      <div class="mb-5">
+        <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
+        <input type="number" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+      </div>
+      <div class="mb-5">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">categoria</label>
+        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+      </div>
+      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/products')
+      .then(response => {
+        console.log(response);
+        this.products = response.data;
+      })
+      .catch(error => console.error('Error al obtener datos:', error));
+  },
+};
+</script>
+
+<style scoped>
+</style>
